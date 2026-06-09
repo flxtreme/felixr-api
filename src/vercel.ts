1e4c4c4c0e2c0464fc4d76c80775fcf894ab0611
Felix Ruz <flxrzjr@gmail.com>
2026-06-09 23:48:44 +0800

    fix: vercel

--- src/vercel.ts
+++ src/vercel.ts
@@ -1,6 +1,6 @@
-import app from '@/app';
+import app from '../src/app';
 
-export default async (req: any, res: any) => {
+export default async function handler(req: any, res: any) {
   await app.ready();
   app.server.emit('request', req, res);
-};
+}
