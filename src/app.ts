2eee1bbcc9eb3f33e2aac8ca77a12f04a4e4ea32
Felix Ruz <flxrzjr@gmail.com>
2026-06-10 08:56:56 +0800

    remove logger

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,8 @@ import cors from '@fastify/cors';
 import { routesPlugin } from '../src/routes/';
 import { swaggerPlugin } from '../src/core/swagger';
 import { config } from '../src/core/config';
-import { loggerOptions } from '../src/core/logger';
 
-const app = Fastify({
-    logger: loggerOptions
-});
+const app = Fastify();
 
 app.register(cors, {
     origin: 'http://localhost:3000'
--- src/vercel.ts
+++ src/vercel.ts
@@ -1,4 +1,4 @@
-import app from '../src/app';
+import app from './app';
 
 export default async (req: any, res: any) => {
   await app.ready();
