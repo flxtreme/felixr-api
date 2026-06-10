79dc946724136da03372a139a43f530ce6d0ee6a
Felix Ruz <flxrzjr@gmail.com>
2026-06-10 12:23:51 +0800

    fix index

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-import app from './app';
-import { config } from './core/config';
+import app from "./app";
+import { config } from "./core/config";
 
-const start = async () => {
-  try {
-    await app.listen({ port: config.port, host: '0.0.0.0' });
-    console.log(`Server running at http://${config.host}:${config.port}`);
-  } catch (err) {
-    console.log(err);
-    process.exit(1);
-  }
-};
-
-start();
+app.listen({ port: config.port, host: '0.0.0.0' }).catch((err) => {
+  console.error(err);
+  process.exit(1);
+}).then(() => {
+  console.log(`Server listening on port ${config.port}`);
+});
