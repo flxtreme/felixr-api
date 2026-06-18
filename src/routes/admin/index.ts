5bbe773a59c378723065c62cb9514c77f44dbb19
Felix Ruz <flxrzjr@gmail.com>
2026-06-18 17:55:58 +0800

    fix: register project endpoint

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -5,6 +5,7 @@ import permissionModule from './permission';
 import tagModule from './tag';
 import userModule from './user';
 import roleModule from './role';
+import projectModule from './project';
 
 const adminModule = async (fastify: FastifyInstance) => {
   const app = fastify.withTypeProvider<TypeBoxTypeProvider>();
@@ -14,6 +15,7 @@ const adminModule = async (fastify: FastifyInstance) => {
   app.register(permissionModule, { prefix: '/permission' });
 
   app.register(postModule, { prefix: '/post' });
+  app.register(projectModule, { prefix: '/project' });
   app.register(tagModule, { prefix: '/tag' });
 };
 
